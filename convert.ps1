dfcddfae7840fa579dc77a11c498fdf62f35e6e7
mattthexc <rahmatkarunia170707@gmail.com>
2026-06-06 16:17:14 +0700

    fix: konversi logo baru ke true PNG agar diterima PWA Builder

--- convert.ps1
+++ convert.ps1
@@ -1,4 +1,4 @@
 Add-Type -AssemblyName System.Drawing
-$img = [System.Drawing.Image]::FromFile('D:\PROJECT RAHMAT\dompetmahasigma\public\icon-512x512.png')
-$img.Save('D:\PROJECT RAHMAT\dompetmahasigma\public\icon-512x512_real.png', [System.Drawing.Imaging.ImageFormat]::Png)
+$img = [System.Drawing.Image]::FromFile('D:\PROJECT RAHMAT\dompetmahasigma\public\icon-1024x1024.png')
+$img.Save('D:\PROJECT RAHMAT\dompetmahasigma\public\icon-1024x1024_real.png', [System.Drawing.Imaging.ImageFormat]::Png)
 $img.Dispose()
